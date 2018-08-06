3565771281fdc7c0d53f8e7babdee8a3e804f59f
nick elliott <Nick.jwl123@gmail.com>
2018-08-06 01:25:58 -0600

    Basic server functionality code

--- myserver.cpp
+++ myserver.cpp
@@ -1,13 +1,14 @@
 #include "myserver.h"
+#include <iostream>
+#include <string>
 
 
 myServer::myServer(QObject *parent) : QObject(parent)
 {
     server = new QTcpServer(this);
-
     connect(server, SIGNAL(newConnection()), this, SLOT(newConnection()));
 
-    if(server->listen(QHostAddress::Any, 1235))
+    if(server->listen(QHostAddress::Any, 1111))
     {
         qDebug() << "Server began running...\n";
     }
@@ -15,19 +16,43 @@ myServer::myServer(QObject *parent) : QObject(parent)
     {
         qDebug() << "Server failed to run\n";
     }
-
-
 }
 
 void myServer::newConnection()
 {
     QTcpSocket* socket = server->nextPendingConnection();
 
-    socket->write("My name is jonas!!!\n\nSend me a message >> ");
+    socket->write("------------------------------\r\nWELCOME TO THE G9 INTERFACE\r\n------------------------------\n ");
+
+    socket->waitForBytesWritten(3000);
 
     socket->flush();
 
+    socket->write("------------------------------\r\nENTER YOU USER NAME\r\n------------------------------\n ");
+
     socket->waitForBytesWritten(3000);
+
+    socket->flush();
+
+
+    // collect data
+    int num_bytes;
+    QByteArray* arr = nullptr;
+
+    do
+    {
+        socket->waitForReadyRead(-1);
+
+        num_bytes = (int)socket->bytesAvailable();
+
+        qDebug() << "Num bytes available: " << num_bytes;
+
+        arr = new QByteArray(socket->readAll(), 1024);
+
+        qDebug() << "Sent data from client:  "<< arr->data();
+
+    }while(arr->toStdString() != "end");
+
 
     socket->close();
 
